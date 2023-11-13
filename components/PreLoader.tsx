c2e7306cf9726cff942202dc39c287b735f91a21
ablfaxl <taghavi.abolfazl@outlook.com>
2023-11-13 23:33:02 +0330

    add animation to PreLaoder component

--- components/PreLoader.tsx
+++ components/PreLoader.tsx
@@ -7,8 +7,8 @@ const PreLoader = () => {
       className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 flex justify-center items-center"
     >
       <div className="flex flex-col gap-3">
-        <div className="w-20 h-20 border-[4px] border-gray-200 border-t-primary-700 rounded-full animate-spin"></div>
-        <div>Loading...</div>
+        <div className="w-20 h-20 border-[4px] border-gray-200 border-t-purple-700 rounded-full animate-spin"></div>
+        <div className="text-lg animate-bounce">Loading...</div>
       </div>
     </div>
   );
